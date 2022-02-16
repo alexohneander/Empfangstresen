e311f27cb1ccc4a20c5a055969a8d63fe584641f
Alex Wellnitz <moin@wellnitz-alex.de>
2022-02-16 21:01:30 +0100

    Add Bull Board queue dashboard.

--- config/server.js
+++ config/server.js
@@ -4,9 +4,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import setRoutes from "./routes";
 
+const { createBullBoard } = require('bull-board')
+const { BullAdapter } = require('bull-board/bullAdapter')
+const jobQueue = require('./../src/queues/JobQueue');
+
+const { router, setQueues, replaceQueues, addQueue, removeQueue } = createBullBoard([
+    new BullAdapter(jobQueue),
+]);
+
 const server = express();
 const compression = require('compression')
-
 
 server.use(bodyParser.json());
 server.use(compression());
@@ -14,5 +21,8 @@ server.use(compression());
 server.disable('x-powered-by');
 
 setRoutes(server);
+
+// Additonal Admin route
+server.use('/admin/queues', router)
 
 export default server;
