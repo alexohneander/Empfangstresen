e7d88a6e6adc5ee5b5bb44dc39c7b6ffff20588f
Alex Wellnitz <moin@wellnitz-alex.de>
2022-02-16 13:06:54 +0100

    Add Status Controller.

---
+++ src/controllers/StatusController.js
@@ -0,0 +1,17 @@
+//src/controllers/StatusController.js
+
+const { flaschenpost } = require('flaschenpost');
+const logger = flaschenpost.getLogger();
+
+class StatusController {
+    constructor() {
+        this.get = this.get.bind(this);
+    }
+
+    get(req, res) {
+        logger.info('Controller: Get Status', { controller: this.constructor.name, method: 'get' });
+        return res.status(200).send({ status: 'ok' });
+    }
+}
+
+export default new StatusController;
