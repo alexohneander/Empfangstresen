8b61c51a8ffde810b2769256f6eb0ba737d888e1
Alex Wellnitz <moin@wellnitz-alex.de>
2022-02-16 13:05:59 +0100

    Add Production Deps.

--- config/server.js
+++ config/server.js
@@ -5,8 +5,13 @@ import bodyParser from "body-parser";
 import setRoutes from "./routes";
 
 const server = express();
+const compression = require('compression')
+
 
 server.use(bodyParser.json());
+server.use(compression());
+
+server.disable('x-powered-by');
 
 setRoutes(server);
 
