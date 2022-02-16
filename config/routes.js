607d0f01c5fdea8cb3a259694b0c29f8eed6d645
Alex Wellnitz <moin@wellnitz-alex.de>
2022-02-16 13:06:44 +0100

    Add new Routes.

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,15 @@
 //config/routes.js
 import ServiceController from './../src/controllers/ServiceController';
+import StatusController from './../src/controllers/StatusController';
 
 export default (server) => {
 
     // SERVICE ROUTES
     server.get(`/api/service`, ServiceController.getAll);
-    server.post(`/api/service`, ServiceController.insert)
+    server.post(`/api/service`, ServiceController.insert);
     server.put(`/api/service/:id`, ServiceController.update);
     server.delete(`/api/service/:id`, ServiceController.delete);
 
+    // STATUS ROUTES
+    server.get(`/status`, StatusController.get);
 }
