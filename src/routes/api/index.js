1e1ac12a3af79c03f50bd7bdcac34b7150dfc3a2
Alex Wellnitz <moin@wellnitz-alex.de>
2022-02-16 08:27:26 +0100

    Add Route.

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,6 +1,7 @@
 var router = require('express').Router();
 
 router.use('/services', require('./services'));
+router.use('/status', require('./status'));
 
 router.use(function(err, req, res, next) {
     if (err.name === 'ValidationError') {
